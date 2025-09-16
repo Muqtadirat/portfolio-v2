4fa273289c3984b9f6f7e5695fd4d7e00f1ef1bb
Muqtadirat <114438425+Muqtadirat@users.noreply.github.com>
2025-09-16 15:48:15 +0100

    feat: analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from 'next';
 import './globals.css';
 import Layout from './layout/Index';
 import { Toaster } from 'sonner';
-import { CustomCursor } from '@/components';
+import { CustomCursor, PostHogProvider } from '@/components';
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata: Metadata = {
   title: 'Muqtadirat Yussuff | Frontend Developer',
@@ -19,13 +20,24 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" type="image/png" sizes="32x32" href="/logo.svg" />
+        {/* <script>
+    !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init Te Ds js Re Os As capture Ke calculateEventProperties zs register register_once register_for_session unregister unregister_for_session Hs getFeatureFlag getFeatureFlagPayload isFeatureEnabled reloadFeatureFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSurveysLoaded onSessionId getSurveys getActiveMatchingSurveys renderSurvey displaySurvey canRenderSurvey canRenderSurveyAsync identify setPersonProperties group resetGroups setPersonPropertiesForFlags resetPersonPropertiesForFlags setGroupPropertiesForFlags resetGroupPropertiesForFlags reset get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted captureException loadToolbar get_property getSessionProperty qs Ns createPersonProfile Bs Cs Ws opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing get_explicit_consent_status is_capturing clear_opt_in_out_capturing Ls debug L Us getPageViewId captureTraceFeedback captureTraceMetric".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+    posthog.init('phc_RiDlJFxLX5i7DThLEVeTCe05ztRUi8E3Pqr4HOxXazE', {
+        api_host: 'https://eu.i.posthog.com',
+        defaults: '2025-05-24',
+        person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+    })
+</script> */}
       </head>
       <body className={`font-roboto`}>
-        <Toaster position="top-center" richColors closeButton />
-        <Layout>
-          <CustomCursor />
-          {children}
-        </Layout>
+        <PostHogProvider>
+          <Toaster position="top-center" richColors closeButton />
+          <Layout>
+            <CustomCursor />
+            {children}
+            <Analytics />
+          </Layout>
+        </PostHogProvider>
       </body>
     </html>
   );
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import CustomCursor from './CustomCursor';
 import Form from './Form';
 import InputField from './InputField';
+import { PostHogProvider } from './PostHogProvider';
 
-export { InputField, Form, CustomCursor };
+export { InputField, Form, CustomCursor,PostHogProvider };
